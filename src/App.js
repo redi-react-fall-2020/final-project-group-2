71d04fd310b99b364aa967cfa614cd21df0294e3
Reda Qero <46996571+RiedAA@users.noreply.github.com>
2020-11-24 21:18:50 +0100

    Started landing page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import logo from './img/logo.svg';
+import italian from './img/Italian.svg';
+import chinese from './img/chinese.svg';
+import indian from './img/indian.svg';
+import mexican from "./img/mexican.svg";
+import thai from "./img/thai.svg";
+import turkish from "./img/turkish.svg";
 import './App.css';
+import {Gateory,HeadingText,
+  HeadingTextBold} from "./StyledComp";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <HeadingText><strong>Support </strong>Local Restuarant, Eat <br/> <strong>Great Food</strong> </HeadingText>
+        <Gateory>
+       <img src={italian} alt="italian Check"/>
+       <p>italian</p>
+        </Gateory>
+        <Gateory>
+       <img src={chinese} alt="chinese Check"/>
+       <p>chinese</p>
+        </Gateory>
+        <Gateory>
+       <img src={thai} alt="thai Check"/>
+       <p>thai</p>
+        </Gateory>
+        <Gateory>
+       <img src={indian} alt="indian Check"/>
+       <p>indian</p>
+        </Gateory>
+        <Gateory>
+       <img src={mexican} alt="mexican Check"/>
+       <p>mexican</p>
+        </Gateory>
+        <Gateory>
+       <img src={turkish} alt="turkish Check"/>
+       <p>turkish</p>
+        </Gateory>
+      
       </header>
     </div>
   );
---
+++ src/StyledComp.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const  Gateory =styled.div`
+width: 86px;
+height: 86px;
+background-color:#FFFFFF;
+`; 
+
+export const  HeadingText =styled.p`
+font-family: 'Rubik', sans-serif;
+color: #743434;
+font-size:40px;
+`; 
